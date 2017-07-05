3a83a379fd59f70c0cfc9211fb2364ce442d9cf5
AroniasPrenovost <Aroniascoder@gmail.com>
2017-07-04 19:45:51 -0700

    first commit

---
+++ jquery.js
@@ -0,0 +1,8 @@
+(function($){
+    $('#myform').submit(function(e){
+        var val = $(this).find('#in').val();
+       $('p.list').append('[' + val + ']' + ' =>  ' + '"' + 'final state here' + '".' + '<br>'); 
+      
+        e.preventDefault(); 
+    });
+})(jQuery);
